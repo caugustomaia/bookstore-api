93e82f12c44c6d7d5f3b385fec6e05234134ce45
Carlos Maia <carlos_augustomaia@hotmail.com>
2023-02-26 15:49:31 -0300

    changed database to mysql

--- models/setup.go
+++ models/setup.go
@@ -1,15 +1,17 @@
 package models
 
 import (
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-
-	"gorm.io/driver/sqlite"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	//database, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+
+	dsn := "user:password@tcp(localhost:3306)/db?charset=utf8mb4&parseTime=True&loc=Local"
+	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database!")
