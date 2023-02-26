0d015f1523e8812728a9e1f3a57e63a8c6fda721
Carlos Maia <carlos_augustomaia@hotmail.com>
2023-02-26 14:01:28 -0300

    first commit

---
+++ controllers/books.go
@@ -0,0 +1,128 @@
+package controllers
+
+import (
+	"bookstore-api/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CreateBookInput struct {
+	Title  string `json:"title" binding:"required"`
+	Author string `json:"author" binding:"required"`
+}
+
+// GET /books
+// Get all books
+func FindBooks(c *gin.Context) {
+	var books []models.Book
+	models.DB.Find(&books)
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": books,
+	})
+}
+
+type UpdateBookInput struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+// GET /books/:id
+// Find a book
+func FindBook(c *gin.Context) {
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Record not found!",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": book,
+	})
+}
+
+// POST /books
+// Create new book
+func CreateBook(c *gin.Context) {
+	// Validate input
+	var input CreateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// Create book
+	book := models.Book{
+		Title:  input.Title,
+		Author: input.Author,
+	}
+	models.DB.Create(&book)
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": book,
+	})
+}
+
+// PATCH /books/:id
+// Update a book
+func UpdateBook(c *gin.Context) {
+	// get model if exists
+	var book models.Book
+
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Record not found!",
+		})
+		return
+	}
+
+	// validate input
+	var input UpdateBookInput
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+	}
+
+	// converter o tipo UpdateBookInput para models.Book
+	b := func(a UpdateBookInput) models.Book {
+		//var b models.Book
+		return models.Book{
+			Title:  a.Title,
+			Author: a.Title,
+		}
+
+	}
+
+	models.DB.Model(&book).Updates(b)
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": book,
+	})
+
+}
+
+// DELETE /books/:id
+// Deleta um livro
+func DeleteBook(c *gin.Context) {
+	// Get model if exists
+	var book models.Book
+	if err := models.DB.Where("id = ?", c.Param("id")).First(&book).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "Record not found!",
+		})
+		return
+	}
+	models.DB.Delete(&book)
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": true,
+	})
+
+}
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bookstore-api/controllers"
+	"bookstore-api/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	models.ConnectDatabase()
+
+	r.GET("/books", controllers.FindBooks)
+	r.GET("/books/:id", controllers.FindBook)
+	r.POST("/books", controllers.CreateBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+
+	r.Run()
+}
